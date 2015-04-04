2479ef830ab225af8e77b14718346b1828955f68
Zoltan Kochan <zoltan.kochan+github@gmail.com>
2015-04-04 18:15:36 +0300

    Tests added.

---
+++ test/package.test.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var expect = require('chai').expect;
+var amqpTransport = require('..');
+var ClientTransport = amqpTransport.ClientTransport;
+var ServerTransport = amqpTransport.ServerTransport;
+
+var options = {
+  amqpURL: 'amqp://guest:guest@localhost:5672'
+};
+var client = new ClientTransport(options);
+var server = new ServerTransport(options);
+
+describe('Transport', function () {
+  it('should send and receive message', function (done) {
+    server.on('foo', function (args, cb) {
+      expect(args[0]).to.be.equal('argsToServer');
+      cb(['argsToClient']);
+    });
+    
+    server.start(function () {
+    });
+    
+    client.connect(function () {
+        client.call('foo', ['argsToServer'], function (args) {
+          expect(args[0]).to.be.equal('argsToClient');
+          done();
+        });
+      });
+  });
+});
